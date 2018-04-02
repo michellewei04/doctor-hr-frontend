b32f7adb20b952ca3638d2b66daa43523c09ad36
Michelle Wei <michellewei12345@gmail.com>
2018-04-02 01:00:45 -0400

    add text field

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
 
 var styles = {
     "dataStyles": {
@@ -10,18 +11,28 @@ var styles = {
     }
 };
 
-const rootURL='http://0.0.0.0:5000';
+const rootURL='http://0.0.0.0:5000/api';
 
 class FetchData extends React.Component {
         constructor() {
             super();
             this.state = {
                 "data": ["nothing yet"],
+                "emailTextField": "",
             };
         }
 
-        getData = () => {
-            axios.get(rootURL+"/api/heart_rate/interval_average").then( (response) => {
+        onNameTextFieldChange = (event) => {
+		// Update the nameTextField state whenever the text field is changed or perturbed in any way:
+		this.setState({"nameTextField": event.target.value});
+	    }
+
+	    onButtonClick = (event) => {
+		console.log(this.state.nameTextField); // log the current nameTextField content
+	    }
+
+        getData = (email) => {
+            axios.get(`${rootURL}/heart_rate/${email}`).then( (response) => {
                 console.log(response);
                 console.log(response.status);
                 this.setState({"data": response.data});
@@ -31,13 +42,13 @@ class FetchData extends React.Component {
         render () {
                 return (
                     <div>
-                            <Button variant="raised" onClick={this.getData}>
-                                Get Data
+                            <TextField
+					                value={this.state.nameTextField}
+					                onChange={this.onNameTextFieldChange}/>
+                            <Button variant="raised" onClick={this.onButtonClick}>
+					                Log text field data.
                             </Button>
-
-                            <div style={styles.dataStyle}>
-                                    {this.state.data[0]}
-                            </div>
+				            {this.state.nameTextField /*show the current nameTextField state here in the browser */}
                      </div>
                 )
         }
